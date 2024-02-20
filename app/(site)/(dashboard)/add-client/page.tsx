fceb27845d3bc9468a54a3a8d35612be8ff96f66
Showkatul islam <showkatul2@gmail.com>
2024-02-20 21:00:42 +0600

    change

--- app/(site)/(dashboard)/add-client/page.tsx
+++ app/(site)/(dashboard)/add-client/page.tsx
@@ -4,7 +4,7 @@ import { AddClient } from './add-client';
 const page = () => {
     return (
         <div className='h-full flex justify-center items-center'>
-           <div className='max-w-5xl'>
+           <div className='w-96'>
                 <AddClient/>
            </div>
         </div>
