2583fc260b3428d60b346706fbc02056e867397b
Showkatul islam <showkatul2@gmail.com>
2024-02-20 22:16:47 +0600

    1.2

--- app/components/Skeleton-Component.tsx
+++ app/components/Skeleton-Component.tsx
@@ -2,7 +2,7 @@
 import { Skeleton } from "@/components/ui/skeleton";
 export const SkeletonTable = () => {
   return (
-    <div className="flex flex-col space-y-4 p-10">
+    <div className="flex flex-col space-y-4 p-10 max-w-5xl">
       <Skeleton className="h-6 w-screen" />
       <Skeleton className="h-6 w-screen" />
       <Skeleton className="h-6 w-screen" />
