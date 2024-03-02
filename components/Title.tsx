cd0b1f6b711b887b1912982f6a1970e5d54f628a
Showkatul islam <showkatul2@gmail.com>
2024-03-02 22:28:15 +0600

    2.3

--- components/Title.tsx
+++ components/Title.tsx
@@ -2,7 +2,7 @@ interface titleprops {
   title: string;
 }
 const Title = ({ title }: titleprops) => {
-  return <div className="text-center uppercase font-bold border-2 p-2 border-zinc-600 shadow-lg bg:bg-zinc-900 bg-white">{title}</div>;
+  return <div className="text-center uppercase font-bold border-2 p-2 border-zinc-600 shadow-lg ">{title}</div>;
 };
 
 export default Title;
